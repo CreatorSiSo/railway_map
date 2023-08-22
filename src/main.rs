fe8998632169fc483f8abf3bf88ff31a404e22d7
Simon Sommer <64036709+CreatorSiSo@users.noreply.github.com>
2023-08-22 04:33:42 +0200

    Fix small progressbar display issues.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use indicatif::MultiProgress;
 use indicatif::ParallelProgressIterator;
 use indicatif::ProgressBar;
+use indicatif::ProgressIterator;
 use indicatif::ProgressStyle;
 use osmpbfreader::{Node, NodeId, OsmObj, OsmPbfReader, Way, WayId};
 use rayon::prelude::*;
@@ -96,9 +97,7 @@ fn main() {
 				.add(ProgressBar::new(region_names.len() as u64))
 				.with_style(
 					ProgressStyle::default_bar()
-						.template(
-							"\n[{duration_precise}] [{wide_bar:.yellow/orange}] {pos}/{len} {eta}",
-						)
+						.template("\n[{elapsed_precise}] [{wide_bar:.yellow/orange}] {pos}/{len}")
 						.unwrap(),
 				)
 				.with_message("Overall");
@@ -145,6 +144,7 @@ fn filter(
 		&osm_pbf_path,
 	)?;
 
+	// TODO progressbars for filtering process
 	let cache_path = PathBuf::from(format!("./cache/{region}.bin"));
 	if force || !cache_path.exists() {
 		let mut reader = OsmPbfReader::new(std::fs::File::open(osm_pbf_path)?);
@@ -211,6 +211,7 @@ async fn download_file(
 
 	let spinner = multi_progress.add(ProgressBar::new_spinner());
 	spinner.set_style(ProgressStyle::with_template("{spinner} {msg}")?);
+	spinner.enable_steady_tick(Duration::from_millis(100));
 
 	spinner.set_message("🔍 Checking whether file exists");
 	let up_to_date = path.exists() && {
@@ -230,7 +231,7 @@ async fn download_file(
 		let mut stream = res.bytes_stream();
 		let mut file = tokio::fs::File::create(&path).await?;
 
-		spinner.set_message(format!("\"{url}\""));
+		spinner.set_message(format!("🚚 \"{url}\""));
 		spinner.set_style(
 			ProgressStyle::with_template(
 				"[{elapsed_precise}] {msg:70} [{wide_bar:.cyan/blue}] {bytes:>12}/{total_bytes:12} {eta:>3}",
@@ -238,6 +239,7 @@ async fn download_file(
 			.progress_chars("##-"),
 		);
 		spinner.set_length(len);
+		spinner.disable_steady_tick();
 
 		while let Some(maybe_chunk) = stream.next().await {
 			let mut chunk = maybe_chunk?;
